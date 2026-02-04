b3a5aa3805212e719f6e663b1bac8f4235792d1c
amanpreet-dhillon <amanpreet.d004@gmail.com>
2026-02-04 13:50:19 -0500

    started implementing gNB

--- include/gNB.h
+++ include/gNB.h
@@ -6,24 +6,34 @@
 #include "Packet.h"
 #include <vector>
 #include <queue>
+#include <map>
 
 class UE;
 
 class gNB : public Node {
     public:
-        gNB();
+        gNB(int,int,int);
         ~gNB();
-        void addUE();
-        void removeUE();
+        void connectUE(UE*);
+        void disconnectUE(UE*);
         void recievePacket(std::unique_ptr<Packet>);
-        void forwardToCore(const CoreNetwork&);
+        void recievePacket(std::unique_ptr<GTPPacket>);
+        void forwardToCore(std::unique_ptr<GTPPacket>);
+        void establishConnectionToCore(const CoreNetwork*);
+        void sendPacket(int, int, PacketType, const std::string&, int);
         //void disconnectUE(int);   /remove?
 
 
     private:
+        const CoreNetwork* coreNetwork;
         std::vector<UE*> connectedUEs;
-        std::priority_queue<Packet> packets;
-
+        //std::priority_queue<Packet> packets;
+        std::map<int, int> recievedPacketTracker; //keep track of packets recieved -- source ID / expected sequence
+        std::map<std::pair<int,int>, std::unique_ptr<Packet>> retransmissionQueue;  //stores packets sent to a specific destination  -- destination ID, expected sequence pair / packet sent
+        std::map<int, int> destinationSeqTracker; //keep track of the number of packets sent to a specific destination ID (i.e. the sequence number)
+        std::map<std::pair<int,int>, std::unique_ptr<Packet>> buffer;
+        void bufferCleanUp(int);
+        
 
 };
 
---
+++ src/gNB.cpp
@@ -0,0 +1,51 @@
+#include "gNB.h"
+#include "UE.h"
+#include <iostream>
+#include <algorithm>
+
+gNB::gNB(int givenID, int xcord, int ycord) : Node(givenID, xcord, ycord){
+
+}
+
+gNB::~gNB(){
+
+}
+
+void gNB::connectUE(UE* ue){
+    connectedUEs.push_back(ue);
+}
+
+void gNB::disconnectUE(UE* ue){
+    auto UElocation = std::find(connectedUEs.begin(), connectedUEs.end(), ue);
+    
+    if (UElocation != connectedUEs.end()){
+        connectedUEs.erase(UElocation);
+    }
+}
+
+void gNB::recievePacket(std::unique_ptr<Packet> data){   //this will come from a UE, wrap, forward to Core Network
+
+   auto wrapped = std::make_unique<GTPPacket>();
+   wrapped->TEID = 80000 + data->getSource();    
+   wrapped->payload = std::move(data);
+   
+   forwardToCore(std::move(wrapped));
+
+}
+
+void gNB::recievePacket(std::unique_ptr<GTPPacket> data){    //this will come from Core Netwwork, unwrap, send to destination UE
+
+}
+
+void gNB::forwardToCore(std::unique_ptr<GTPPacket> payload){    
+    //coreNetwork->recievePacket(std::move(payload))
+}
+
+void gNB::establishConnectionToCore(const CoreNetwork* cN){
+    coreNetwork = cN;
+}
+
+void gNB::sendPacket(int destID, int seq, PacketType pType, const std::string& data, int prio=0){
+
+}
+
